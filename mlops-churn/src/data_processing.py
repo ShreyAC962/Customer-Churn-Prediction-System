679704e8df316b16335ab1353f24fd7bc9f27530
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-28 12:13:40 -0700

    Fix project structure and remove submodule issue

---
+++ mlops-churn/src/data_processing.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+def preprocess(df):
+    df = df.dropna() # Removes missing values(NaN, None)
+
+    le = LabelEncoder()
+
+    for col in df.select_dtypes(include=['object']):
+        df[col] = le.fit_transform(df[col])
+
+
+    X = df.drop("Chun", axis = 1) # Removes the col(axis = 1) that have lable of the column "Churn"
+    y = df["Chun"] # Selects only data from "Churn" column of the original (DataFrame- df)
+
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
